70c77d47dc31e0ccb1eff48305f9b0011ab5038f
arthurdef <arthur.defauconpret@free.fr>
2024-06-21 15:16:26 +0200

    charger geojson avec api

--- scripts/map.js
+++ scripts/map.js
@@ -1,16 +1,26 @@
-// Initialiser la carte
-var map = L.map('map').setView([48.8566, 2.3522], 13); // Coordonnées de Paris
+// Initialisation de la carte
+var map = L.map('map').setView([46.9, 3.5], 6); 
 
-// Ajouter une couche de tuiles OSM
+// Ajout tuiles OSM
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-// Charger et afficher le fichier GeoJSON
-axios.get('path/to/your/file.geojson')
-    .then(function (response) {
-        L.geoJSON(response.data).addTo(map);
+
+// URL de la fonction API geojson sur Vercel
+var geojsonUrl = 'https://get-me-home-back.vercel.app/api/geojson';
+
+// Charger et afficher le GeoJSON depuis la fonction API
+fetch(apiEndpoint)
+    .then(function(response) {
+        if (!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
+        return response.json();
     })
-    .catch(function (error) {
-        console.log(error);
+    .then(function(data) {
+        L.geoJSON(data).addTo(map);
+    })
+    .catch(function(error) {
+        console.log('There has been a problem with your fetch operation:', error);
     });
